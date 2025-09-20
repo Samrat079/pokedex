5cdeb74b25fbdafc199833666b07a1e06285872b
Samrat079 <samratdutta079@gmail.com>
2025-09-21 02:47:27 +0530

    fixed protected rougts

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -40,7 +40,8 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') 
+    request.nextUrl.pathname !== '/' &&
+    !request.nextUrl.pathname.startsWith('/login')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
